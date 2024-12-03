7ab4adf808e8127f33fe9fcf6931b30753bdaf54
MasonMerry11 <tuq98987@temple.edu>
2024-12-03 14:21:44 -0500

    Add files via upload

---
+++ enemy_data.py
@@ -0,0 +1,162 @@
+ENEMY_SPAWN_DATA = [
+ {
+    #1
+    "weak": 15,
+    "medium": 0,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #2
+    "weak": 30,
+    "medium": 0,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #3
+    "weak": 20,
+    "medium": 5,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #4
+    "weak": 30,
+    "medium": 15,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #5
+    "weak": 5,
+    "medium": 20,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #6
+    "weak": 15,
+    "medium": 15,
+    "strong": 4,
+    "elite": 0
+  },
+  {
+    #7
+    "weak": 20,
+    "medium": 25,
+    "strong": 5,
+    "elite": 0
+  },
+  {
+    #8
+    "weak": 10,
+    "medium": 20,
+    "strong": 15,
+    "elite": 0
+  },
+  {
+    #9
+    "weak": 15,
+    "medium": 10,
+    "strong": 5,
+    "elite": 0
+  },
+  {
+    #10
+    "weak": 0,
+    "medium": 100,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #11
+    "weak": 5,
+    "medium": 10,
+    "strong": 12,
+    "elite": 2
+  },
+  {
+    #12
+    "weak": 0,
+    "medium": 15,
+    "strong": 10,
+    "elite": 5
+  },
+  {
+    #13
+    "weak": 20,
+    "medium": 0,
+    "strong": 25,
+    "elite": 10
+  },
+  {
+    #14
+    "weak": 15,
+    "medium": 15,
+    "strong": 15,
+    "elite": 15
+  },
+  {
+    #15
+    "weak": 25,
+    "medium": 25,
+    "strong": 25,
+    "elite": 25
+  },
+  {
+    #16
+    "weak": 30,
+    "medium": 26,
+    "strong": 26,
+    "elite": 26 
+  },
+  {
+    #17
+    "weak": 35,
+    "medium": 28,
+    "strong": 27,
+    "elite": 26 
+  },
+  {
+    #18
+    "weak": 40,
+    "medium": 30,
+    "strong": 30,
+    "elite": 28  
+  },
+  {
+    #19
+    "weak": 40,
+    "medium": 29,
+    "strong": 29,
+    "elite": 30 
+  },
+  {
+    #20
+    "weak": 45,
+    "medium": 32,
+    "strong": 32,
+    "elite": 30  
+  }
+]
+
+ENEMY_DATA = {
+    "weak": {
+    "health": 20,
+    "speed": 2
+  },
+    "medium": {
+    "health": 35,
+    "speed": 3
+  },
+    "strong": {
+    "health": 50,
+    "speed": 4
+  },
+    "elite": {
+    "health": 70,
+    "speed": 6
+  }
+}
+	
